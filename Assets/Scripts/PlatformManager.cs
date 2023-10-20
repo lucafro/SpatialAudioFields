0cebfe0f390b3d9a953886e5704876aa289245fe
lucafro <lucafrosler@gmail.com>
2023-10-20 16:09:34 +0200

    Added Unity Project

---
+++ Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    public GameObject FPSController;
+    public GameObject VRPlayer;
+
+    void Awake()
+    {
+#if UNITY_EDITOR
+        FPSController.SetActive(true);
+        VRPlayer.SetActive(false);
+#endif
+
+#if UNITY_ANDROID
+        VRPlayer.SetActive(true);
+        FPSController.SetActive(false);
+        //UnityEngine.XR.XRSettings.eyeTextureResolutionScale = 2.0f;
+#endif
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TeleportManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+public class TeleportManager : MonoBehaviour
+{
+    public static event Action<Transform> DoTeleport;
+    [SerializeField] VRInteractiveItem[] teleportLocations;
+    [SerializeField] Transform reticleTransform;
+
+    void OnEnable()
+    {
+        foreach (VRInteractiveItem t in teleportLocations)
+        {
+            t.OnClick += Teleport;
+        }
+    }
+
+    void OnDisable()
+    {
+        foreach (VRInteractiveItem t in teleportLocations)
+        {
+            t.OnClick -= Teleport;
+        }
+    }
+
+    void Teleport()
+    {
+        if (DoTeleport != null)
+        {
+            DoTeleport(reticleTransform);
+        }
+        else
+        {
+            Debug.Log("DoTeleport event has no subscribers.");
+        }
+    }
+}
