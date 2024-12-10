06d3806c8d38032e6808e7ea7a3c365ed23219a3
lucafro <lucafrosler@gmail.com>
2024-12-10 23:19:38 +0100

    Refactored PlatformManager

--- Assets/_CustomAssets/Scripts/Runtime/PlatformManager.cs
+++ Assets/_CustomAssets/Scripts/Runtime/PlatformManager.cs
@@ -1,23 +1,34 @@
 ﻿using UnityEngine;
 
-public class PlatformManager : MonoBehaviour
+/// <summary>
+/// Automatically switch between XR rig and FPS controller depending on platform.
+/// </summary>
+
+namespace Lucafro.Runtime
 {
-    [SerializeField]
-    private GameObject _XRRig;
-    [SerializeField]
-    private GameObject _FPSController;
-
-    private void OnEnable()
+    public class PlatformManager : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField, Tooltip("Reference to the XR camera rig.")]
+        private GameObject _XRRig;
+        [SerializeField, Tooltip("Reference to FPS controller.")]
+        private GameObject _FPSController;
 
+        private void Start()
+        {
 #if UNITY_EDITOR
-        _FPSController?.SetActive(true);
-        _XRRig?.SetActive(false);
-
+            if (_XRRig && _FPSController)
+            {
+                _XRRig.SetActive(false);
+                _FPSController.SetActive(true);
+            }
 #elif UNITY_ANDROID
-        _XRRig?.SetActive(true);
-        _FPSController?.SetActive(false);
+        if (_XRRig && _FPSController)
+        {
+            _XRRig.SetActive(true);
+            _FPSController.SetActive(false);
+        }
 #endif
-
+        }
     }
 }
